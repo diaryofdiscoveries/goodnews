3d4126058433c3fcf6af978d5e82b88f94641894
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-10-28 21:37:58 +0100

    Update tests

--- src/article-filter.js
+++ src/article-filter.js
@@ -8,7 +8,7 @@ function ArticleFilter(unfilteredData){
 ArticleFilter.prototype.filterByTitle = function() {
   this.unfilteredData.filter((article) => {
     var articleTitle = article.title;
-    var words = ["Trump", "Harassment", "Groped", "Groping", "Murder", "Murderer", "Murdered", "Rape", "Raping", "Rapist", "War", "Battle", "Demands", "Demand", "Killing", "Killer", "Killed", "Brexit", "Racist", "Racism", "Misogynist", "Assault", "Weinstein", "Attack", "Attacker", "Attacks", "ISIS", "Terrorism", "Terrorist", "Conspiracy", "Fallout", "Sad", "Bad"];
+    var words = ["Trump", "Harassment", "Groped", "Groping", "Murder", "Murderer", "Murdered", "Rape", "Raping", "Rapist", "Putin", "Missiles", "War", "Battle", "Defiant", "Deaths", "Demands", "Demand", "Killing", "Killer", "Killed", "Brexit", "Racist", "Racism", "Misogynist", "Assault", "Weinstein", "Attack", "Attacker", "Attacks", "ISIS", "Terrorism", "Terrorist", "Conspiracy", "Fallout", "Sad", "Bad"];
     console.log(matcher(articleTitle, words));
     console.log((matcher(articleTitle, words)).length === 0);
     if ((matcher(articleTitle, words)).length === 0)
